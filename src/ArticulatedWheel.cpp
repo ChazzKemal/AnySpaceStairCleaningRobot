d184ee2f1e58078907458044b4d8c2c54999f49f
ChazzKemal <go25guh@mytum.de>
2025-09-15 20:10:45 +0200

    merge the jascha branch

--- src/ArticulatedWheel.cpp
+++ src/ArticulatedWheel.cpp
@@ -1,14 +1,15 @@
 #include "ArticulatedWheel.h"
-
+#include <Arduino.h>
 // The constructor now takes pin numbers and initializes the AccelStepper objects directly.
-ArticulatedWheel::ArticulatedWheel(FastAccelStepperEngine* engine,uint8_t drive_step_pin, uint8_t drive_dir_pin,
+ArticulatedWheel::ArticulatedWheel(FastAccelStepperEngine *engine, uint8_t drive_step_pin, uint8_t drive_dir_pin,
                                    uint8_t steer_step_pin, uint8_t steer_dir_pin,
-                                   uint8_t height_step_pin, uint8_t height_dir_pin,
+                                   uint8_t height_step_pin, uint8_t height_dir_pin, uint8_t _homingPin,
                                    bool invert_drive, bool invert_steer, bool invert_height)
 {
-    drive =  new Stepper(engine,drive_step_pin,drive_dir_pin,invert_drive);
-    steer =  new Stepper(engine,steer_step_pin,steer_dir_pin,invert_steer);
-    height =  new Stepper(engine,height_step_pin,height_dir_pin,invert_height);
+    drive = new Stepper(engine, drive_step_pin, drive_dir_pin, invert_drive);
+    steer = new Stepper(engine, steer_step_pin, steer_dir_pin, invert_steer);
+    height = new Stepper(engine, height_step_pin, height_dir_pin, invert_height);
+    homingPin = _homingPin;
 }
 
 void ArticulatedWheel::begin(float max_drive_speed,
@@ -17,45 +18,63 @@ void ArticulatedWheel::begin(float max_drive_speed,
                              float acceleration_drive,
                              float acceleration_steer,
                              float acceleration_height)
-{   // Those are the Default Speeds
-    drive->init(max_drive_speed,acceleration_drive,1);
-    steer->init(max_steer_speed,acceleration_steer,1);
-    height->init(max_height_speed,acceleration_height,1);
+{ // Those are the Default Speeds
+    drive->init(max_drive_speed, acceleration_drive, 1);
+    steer->init(max_steer_speed, acceleration_steer, 1);
+    height->init(max_height_speed, acceleration_height, 1);
+    pinMode(homingPin, INPUT_PULLUP);
 }
 
+bool ArticulatedWheel::checkHomingPin()
+{
+    return (!digitalRead(homingPin));
+}
 
-
-
-
-Stepper::Stepper(FastAccelStepperEngine* engine,uint8_t step_pin, uint8_t dir_pin,bool invert){
+Stepper::Stepper(FastAccelStepperEngine *engine, uint8_t step_pin, uint8_t dir_pin, bool invert)
+{
     _stepper = engine->stepperConnectToPin(step_pin);
     invertDir = invert;
-    if (_stepper){
+    if (_stepper)
+    {
         _stepper->setDirectionPin(dir_pin);
         Serial.println("initialization worked");
     }
-    else{
-        
+    else
+    {
+
         Serial.println("Stepper initialization failed");
-    
     }
 }
 
-void Stepper::init(float speed,float acceleration,float conversionfact){
+void Stepper::init(float speed, float acceleration, float conversionfact)
+{
     default_Speed = speed;
     default_acceleration = acceleration;
-    conversionFactor    = conversionfact;
+    conversionFactor = conversionfact;
+    position = 0;
     _stepper->setAcceleration(acceleration); // steps/s^2
-    _stepper->setSpeedInHz(speed);     // steps/s
-
+    _stepper->setSpeedInHz(speed);           // steps/s
 }
 
-void Stepper:: moveSteps(int steps){
-    Serial.println("doing steps ");
- 
+void Stepper::moveSteps(int steps)
+{
+
     _stepper->move(steps);
 }
 
-int Stepper::convertToSteps(float moveBy){
-    return conversionFactor * moveBy;
+int Stepper::convertToSteps(float moveBy)
+{
+    return (int)conversionFactor * moveBy;
+}
+
+void Stepper::moveToPosition(float newPosition)
+{
+    moveSteps(convertToSteps(newPosition - position));
+    position = newPosition;
+}
+
+void Stepper::moveRelative(float relPos)
+{
+    moveSteps(convertToSteps(relPos));
+    position += relPos;
 }
